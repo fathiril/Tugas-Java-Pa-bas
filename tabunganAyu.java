37042baab9853216741a05e5acb6355db6821561
Fathir <fathirilhaqfau@gmail.com>
2025-08-07 09:38:51 +0800

    Add files via upload

---
+++ tabunganAyu.java
@@ -0,0 +1,13 @@
+public class tabunganAyu {
+    public static void main(String[] args) {
+        int saldo = 0; // saldo awal
+        int tabungan;
+
+        for (int hari = 1; hari <= 30; hari++) {
+            tabungan = hari * 1000; // menabung Rp1000 per hari
+            saldo += tabungan;
+        }
+
+        System.out.println("Saldo ayu di hari ke 30: " + saldo);
+    }
+}
